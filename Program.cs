7c926a5790ba53b5005732ced902668cc2ead587
Nastaran <Nastaran@DESKTOP-B5I9M7N>
2023-06-04 15:13:43 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine(long.MaxValue);
+System.Console.WriteLine(long.MaxValue.ToString("#,##0"));
+
+System.Console.Write("What is you number: ");
+string maxNumberString = System.Console.ReadLine();
+long maxNumber = System.Convert.ToInt64(maxNumberString);
+
+long primeNumberCount = 0;
+
+for (long number = 1; number <= maxNumber; number++)
+{
+    bool isPrimeNumber = true;
+
+    for (long index = 2; index <= number - 1; index++)
+    {
+        if (number % index == 0)
+        {
+            isPrimeNumber = false;
+
+            break;
+        }
+    }
+
+    if (isPrimeNumber)
+    {
+        primeNumberCount++;
+        System.Console.WriteLine(number);
+    }
+}
+
+System.Console.WriteLine($"The prime number count is {primeNumberCount}");
